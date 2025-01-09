c6f8312b54983248c78e2afbed46151eec987227
Andrew Chu <67875282+andrewchu16@users.noreply.github.com>
2025-01-09 12:56:51 -0500

    Remove unnecessary background color

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Create, Error404, Join } from "./pages";
-import Header from "./components/layout/Header";
 import "react-material-symbols/rounded";
+import Header from "./components/layout/Header";
 
 function App() {
     return (
         <div className="app">
             <BrowserRouter>
                 <Header />
-                <div className="bg-blue-50">
-                    <Routes>
-                        <Route path="/" element={<Create />} />
-                        <Route path=":meetId" element={<Join />} />
-                        <Route path="*" element={<Error404 />} />
-                    </Routes>
-                </div>
+                <Routes>
+                    <Route path="/" element={<Create />} />
+                    <Route path=":meetId" element={<Join />} />
+                    <Route path="*" element={<Error404 />} />
+                </Routes>
             </BrowserRouter>
         </div>
     );
