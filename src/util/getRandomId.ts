2a7034fea8263a2e0365493bf26e8c5088064d58
Andrew Chu <67875282+andrewchu16@users.noreply.github.com>
2025-01-10 13:12:57 -0500

    Refactor random ID generator

--- src/util/getRandomId.ts
+++ src/util/getRandomId.ts
@@ -1,5 +1,21 @@
-const getRandomId = () => {
-    return (Math.random() + 1).toString(36).substring(7);
-}
+const getRandomId = (length = 5) => {
+    const dateString = Date.now().toString(36);
+    const randomness = Math.random().toString(36).substring(2);
+
+    // interleave date and randomness character by character
+    let result = "";
+    for (let i = 0; i < dateString.length; i++) {
+        result += dateString[i];
+        if (i < randomness.length) {
+            result += randomness[i];
+        }
+    }
+
+    while (result.length < length) {
+        result += Math.random().toString(36).substring(2);
+    }
+
+    return result.substring(0, length);
+};
 
 export { getRandomId };
