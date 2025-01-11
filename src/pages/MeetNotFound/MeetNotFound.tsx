e673c9511231374d6f03554731a1ae0fac12363f
Andrew Chu <67875282+andrewchu16@users.noreply.github.com>
2025-01-10 23:30:02 -0500

    Fix type error

--- src/pages/MeetNotFound/MeetNotFound.tsx
+++ src/pages/MeetNotFound/MeetNotFound.tsx
@@ -2,16 +2,25 @@ import { MaterialSymbol } from "react-material-symbols";
 import { Contact } from "../../components/modules";
 
 interface MeetNotFoundProps {
-    id: string;
+    id: string | undefined;
 }
 
 const MeetNotFound = ({ id }: MeetNotFoundProps) => {
     return (
         <div className="flex flex-col items-center justify-center h-screen w-screen absolute top-0 left-0 p-10 gap-2">
-            <MaterialSymbol icon="question_mark" size={40} className="text-primary mb-4" weight={900} />
-            <h1 className="text-dark text-center font-bold text-2xl">Meet Not Found</h1>
+            <MaterialSymbol
+                icon="question_mark"
+                size={40}
+                className="text-primary mb-4"
+                weight={900}
+            />
+            <h1 className="text-dark text-center font-bold text-2xl">
+                Meet Not Found
+            </h1>
             <p className="text-body text-center">
-                Meet <span className="text-dark">{id}</span> could not be found. <Contact />
+                Meet{" "}
+                <span className="text-dark">{id !== undefined ? id : ""}</span>{" "}
+                could not be found. <Contact />
             </p>
         </div>
     );
