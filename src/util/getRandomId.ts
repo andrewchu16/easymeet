13efbee8515a4dbb19c901fb58c99dc704bacd93
Andrew Chu <67875282+andrewchu16@users.noreply.github.com>
2025-01-11 02:15:39 -0500

    Fix random id generation

--- src/util/getRandomId.ts
+++ src/util/getRandomId.ts
@@ -1,15 +1,5 @@
 const getRandomId = (length = 5) => {
-    const dateString = Date.now().toString(36);
-    const randomness = crypto.randomUUID().replace(/-/g, "");
-
-    // interleave date and randomness character by character
-    let result = "";
-    for (let i = 0; i < dateString.length; i++) {
-        result += dateString[i];
-        if (i < randomness.length) {
-            result += randomness[i];
-        }
-    }
+    let result = (Math.random() + 1).toString(36).substring(7);
 
     while (result.length < length) {
         result += Math.random().toString(36).substring(2);
